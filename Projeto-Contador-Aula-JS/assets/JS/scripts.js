02686dba9ccc21f0bbae3ee20d769c4dfd41385b
TiagoMata <56051094+TiagoMata@users.noreply.github.com>
2022-06-15 16:58:48 -0300

    Concluindo-calculado.js

--- Projeto-Contador-Aula-JS/assets/JS/scripts.js
+++ Projeto-Contador-Aula-JS/assets/JS/scripts.js
@@ -9,4 +9,21 @@ function increment(){
 function decrement(){
   currentNumber = currentNumber - 1;
   currentNumberWrapper.innerHTML = currentNumber;
+  
 }
+
+function count(){
+  (count <= -1);
+  count === false;
+}
+document.getElementById("#menos").addEventListener('click', count);
+
+function compara(num1, num2){
+
+  if (num1 == num2){
+     return true;
+  }
+ 
+  console.log(compara(10, 10));
+}
+
